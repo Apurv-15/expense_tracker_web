f52ef343973038c3aa935ccb6df621e815d93d4e
Apurv <apurvd16@gmail.com>
2025-04-25 19:18:12 +0530

    full working code

--- expense/src/main.jsx
+++ expense/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Auth0Provider } from "@auth0/auth0-react";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import './index.css';
 import App from './App';
 
@@ -20,6 +21,7 @@ root.render(
     >
       <App />
       <Analytics />
+      <SpeedInsights />
     </Auth0Provider>
   </React.StrictMode>
 );
